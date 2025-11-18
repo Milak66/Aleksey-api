bd0091bd81aae8fd040740f70f21e2be41fa888d
Aleksey <134363714+Milak66@users.noreply.github.com>
2025-11-18 16:24:28 +0530

    Aleksey api 1.1.1 test

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ app.route('/getData')
 
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.EMAIL,
             pass: process.env.PASSWORD,
