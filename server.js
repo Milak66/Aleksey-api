4ad78cd446b4db72030e0652406b6ae8efeab321
Aleksey <134363714+Milak66@users.noreply.github.com>
2025-11-24 16:18:21 +0530

    server test1

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ console.log(process.env);
 const app = express();
 const PORT = 5696;
 
+const data = {
+    message: 'here is your data, punk'
+}
+
 app.use(cors({
     origin: 'https://aleksey-the-developer.vercel.app',
     credentials: true
@@ -19,39 +23,44 @@ app.get('/', (req, res) => {
     res.send("Server started");
 });
 
-app.route('/getData')
-.get((req, res) => {
-    res.send("Getting data");
-})
-.post(async (req, res) => {
-    const { userData } = req.body;
-    console.log('Received userData:', userData);
-
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.PASSWORD,
-        },
-    });
-
-    const mailOptions = {
-        from: `"Заказчик" <${process.env.EMAIL}>`,
-        to: process.env.EMAIL,
-        subject: 'Новое сообщение от пользователя',
-        text: `Пользователь написал: ${userData}`,
-    };
-
-    try {
-        await transporter.sendMail(mailOptions);
-        res.json({ message: 'Данные успешно отправлены и письмо отправлено' });
-    } catch (error) {
-        console.error('Ошибка при отправке письма:', error);
-        res.status(500).json({ message: 'Ошибка при отправке почты', error: error.toString() });
-    }
+app.get('/getData', (req, res) => {
+    const jsonData = JSON.stringify(data);
+    res.send(jsonData);
 });
+
+// app.route('/getData')
+// .get((req, res) => {
+//     res.send("Getting data");
+// })
+// .post(async (req, res) => {
+//     const { userData } = req.body;
+//     console.log('Received userData:', userData);
+
+//     const transporter = nodemailer.createTransport({
+//         host: 'smtp.gmail.com',
+//         port: 465,
+//         secure: true,
+//         auth: {
+//             user: process.env.EMAIL,
+//             pass: process.env.PASSWORD,
+//         },
+//     });
+
+//     const mailOptions = {
+//         from: `"Заказчик" <${process.env.EMAIL}>`,
+//         to: process.env.EMAIL,
+//         subject: 'Новое сообщение от пользователя',
+//         text: `Пользователь написал: ${userData}`,
+//     };
+
+//     try {
+//         await transporter.sendMail(mailOptions);
+//         res.json({ message: 'Данные успешно отправлены и письмо отправлено' });
+//     } catch (error) {
+//         console.error('Ошибка при отправке письма:', error);
+//         res.status(500).json({ message: 'Ошибка при отправке почты', error: error.toString() });
+//     }
+// });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
